87a1adee8602e8d9f53db512116d64dd4c05a8b2
GermanVq <gavegaq@gmail.com>
2020-03-14 02:08:39 -0500

    Resistor color

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,18 @@
+encode =  {'black': 0,
+       'brown': 1,
+       'red': 2,
+       'orange': 3,
+       'yellow': 4,
+       'green': 5,
+       'blue': 6,
+       'violet': 7,
+       'grey': 8,
+       'white': 9
+       }
+
+def color_code(color):
+   return encode[color]
+
+
+def colors():
+    return list(encode.keys())
